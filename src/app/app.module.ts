1141ec4d2b87210cc8098ac6b8bce02e86ecae89
Marcos Carneiro Lima <marcos.lima.crn@gmail.com>
2017-01-11 10:52:47 -0300

    feat(app.module): Add the import of angular2-swing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,16 @@ import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
+import { SwingModule } from 'angular2-swing';
+
 @NgModule({
   declarations: [
     MyApp,
     HomePage
   ],
   imports: [
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    SwingModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
