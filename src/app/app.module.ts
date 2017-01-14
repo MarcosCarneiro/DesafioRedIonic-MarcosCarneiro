8637e37b412bb027a35076d99b9fd724cb735a18
Marcos Carneiro Lima <marcos.lima.crn@gmail.com>
2017-01-13 22:38:05 -0300

    feat(slide-modal): Add files of the slide modal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
+import { SlideModal } from '../pages/slide-modal/slide-modal';
 
 import { SwingModule } from 'angular2-swing';
 
@@ -14,7 +15,8 @@ import { SwingModule } from 'angular2-swing';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    SlideModal
   ],
   imports: [
     IonicModule.forRoot(MyApp),
@@ -26,7 +28,8 @@ import { SwingModule } from 'angular2-swing';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    SlideModal
   ],
   providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
 })
---
+++ src/pages/slide-modal/slide-modal.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+
+@Component({
+  selector: 'page-slide-modal',
+  templateUrl: 'slide-modal.html'
+})
+export class SlideModal {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+
+}
