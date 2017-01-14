3243d7b41b5be5c44efc635b839b75e23695e9d6
Marcos Carneiro Lima <marcos.lima.crn@gmail.com>
2017-01-14 04:59:28 -0300

    feat(): Add slide and buttons in the template slide-modal

--- src/pages/slide-modal/slide-modal.ts
+++ src/pages/slide-modal/slide-modal.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams, ViewController } from 'ionic-angular';
 
+import { User } from '../../model/user';
+
 
 @Component({
   selector: 'page-slide-modal',
@@ -8,12 +10,29 @@ import { NavController, NavParams, ViewController } from 'ionic-angular';
 })
 export class SlideModal {
 
+  slides : Array<any>;
+  card = new User;
+
   constructor(public navCtrl: NavController, private navParams: NavParams, private viewCtrl: ViewController) {
-    console.log('User Picture: ' + navParams.get('userPicture'));
+    this.slides = [];
+    this.card = this.navParams.get('card');
+    this.requirePictures();
   }
 
-  dismiss(){
-    this.viewCtrl.dismiss('testDismiss');
+  requirePictures() {
+    this.slides.push(this.card.getPicture());
+
+    this.slides.push('http://lorempixel.com/640/427/sports/');
+    this.slides.push('http://lorempixel.com/640/427/city/');
+    this.slides.push('http://lorempixel.com/640/427/fashion/');
+  }
+
+  dismiss(vote){
+    this.viewCtrl.dismiss(vote);
+  }
+
+  close(){
+    this.navCtrl.pop();
   }
 
 }
